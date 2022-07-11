e9491f47eab0ac27df962d94e49d73e7a73fe142
AlenaKarpolenko <alenac190590@mail.ru>
2022-07-12 00:50:40 +0700

    added

--- ci-template/src/js/__tests__/Character.test.js
+++ ci-template/src/js/__tests__/Character.test.js
@@ -1,18 +1,23 @@
-import Zombie from '../Zombie';
+import Character from '../Character';
 
-test('should check the class Zombie', () => {
-  class TestZombie {
+test('should check the class Character', () => {
+  class TestCharacter {
     constructor(name) {
       this.name = name;
       this.health = 100;
       this.level = 1;
-      this.type = 'Zombie';
-      this.attack = 40;
-      this.defence = 10;
     }
   }
 
-  const received = new TestZombie('Zombie');
-  const expected = new Zombie('Zombie');
+  const received = new TestCharacter('Игрок');
+  const expected = new Character('Игрок');
   expect(received).toEqual(expected);
+});
+
+test('should check the class Character no name', () => {
+  function errorName() {
+    const testName = new Character();
+    return testName;
+  }
+  expect(errorName).toThrowError(Error);
 });
